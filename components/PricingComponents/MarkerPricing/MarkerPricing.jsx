886208f1ef901a69eb3688dcec38f6fcf43100fe
Stanislav Kintoriak <kintoriak@gmail.com>
2024-01-12 03:13:15 +0200

    commit 1
    1) Pricing folders and sceleton;
    2) Hero with video;
    3) Some styles experiments;

---
+++ components/PricingComponents/MarkerPricing/MarkerPricing.jsx
@@ -0,0 +1,14 @@
+// import css from "./MarkerPricing.module.scss";
+
+// const MarkerPricing = ({dark}) => {
+//     return (<>
+//         {/* <svg className={css.marker} fill={"white"} width={"10px"} height={"10px"}>
+//           <use href={"/logo-sprite.svg#****"} />
+//         </svg>
+//         </> */}
+//     );
+// };
+
+// export default MarkerAndText;
+
+
