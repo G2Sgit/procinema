3a2b5cc2b379c2c16dbdfb7dc29f560040c4ad40
Aleksey-Dr <aprel181818@gmail.com>
2024-01-29 18:34:50 +0200

    Add grid

---
+++ components/HomePageComponents/ShowrealSection/ShowrealSection.jsx
@@ -0,0 +1,81 @@
+import Section from "@/components/Section/Section";
+import Container from "@/components/Container/Container";
+
+import css from "./ShowrealSection.module.scss";
+
+const ShowrealSection = () => {
+    return (
+        <div className={css.wrapper}>
+            <Section>
+                <h2 className={css["visually_hidden"]}>Showreal</h2>
+                <div className={css.parent}>
+                    <div className={css["video__item--1"]}>
+                        <div className={css.overlay}>
+                            <h3>VIP Auto Tires & Service</h3>
+                            <ul>
+                                <li>eCommerce</li>
+                                <li>Magento</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={css["video__item--2"]}>
+                        <div className={css.overlay}>
+                            <h3>VIP Auto Tires & Service</h3>
+                            <ul>
+                                <li>eCommerce</li>
+                                <li>Magento</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={css["video__item--3"]}>
+                        <div className={css.overlay}>
+                            <h3>VIP Auto Tires & Service</h3>
+                            <ul>
+                                <li>eCommerce</li>
+                                <li>Magento</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={css["video__item--4"]}>
+                        <div className={css.overlay}>
+                            <h3>VIP Auto Tires & Service</h3>
+                            <ul>
+                                <li>eCommerce</li>
+                                <li>Magento</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={css["video__item--5"]}>
+                        <div className={css.overlay}>
+                            <h3>VIP Auto Tires & Service</h3>
+                            <ul>
+                                <li>eCommerce</li>
+                                <li>Magento</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={css["video__item--6"]}>
+                        <div className={css.overlay}>
+                            <h3>VIP Auto Tires & Service</h3>
+                            <ul>
+                                <li>eCommerce</li>
+                                <li>Magento</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={css["video__item--7"]}>
+                        <div className={css.overlay}>
+                            <h3>VIP Auto Tires & Service</h3>
+                            <ul>
+                                <li>eCommerce</li>
+                                <li>Magento</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </Section>
+        </div>
+    );
+};
+
+export default ShowrealSection;
