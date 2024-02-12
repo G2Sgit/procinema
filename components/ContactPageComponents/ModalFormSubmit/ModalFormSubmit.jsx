534a0ba5fd6b9e3c78b4ba9b9b3201d5f65189ee
Serhiy Slipchuk <slipchuks@ukr.net>
2024-02-13 00:52:12 +0200

    Fix contacts modal

--- components/ContactPageComponents/ModalFormSubmit/ModalFormSubmit.jsx
+++ components/ContactPageComponents/ModalFormSubmit/ModalFormSubmit.jsx
@@ -2,7 +2,17 @@ import css from "./ModalFormSubmit.module.scss";
 import { HiMiniDocumentCheck } from "react-icons/hi2";
 import { HiMiniXCircle } from "react-icons/hi2";
 
-const ModalFormSubmit = ({ submit, text, buttonText, closeHandler }) => {
+const ModalFormSubmit = ({ submit, text, buttonText, closeFormModal, closeHandler }) => {
+  const onClickHandler = () => {
+    if (closeFormModal) {
+      closeFormModal();
+      closeHandler();
+    } else {
+      closeHandler()
+    }
+  } 
+
+
   return (
     <div className={css.backdrop}>
       <div className={css.modal}>
@@ -13,7 +23,7 @@ const ModalFormSubmit = ({ submit, text, buttonText, closeHandler }) => {
         )}
 
         <p className={css.message}>{text}</p>
-        <button className={css.button} onClick={closeHandler}>
+        <button className={css.button} onClick={onClickHandler}>
           {buttonText}
         </button>
       </div>
