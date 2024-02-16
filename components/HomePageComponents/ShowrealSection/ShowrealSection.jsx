d35c8bff9a74bf7e9877f30ef6a1a9ab2a91ab56
Aleksey-Dr <aprel181818@gmail.com>
2024-02-16 18:42:30 +0200

    Add grid

--- components/HomePageComponents/ShowrealSection/ShowrealSection.jsx
+++ components/HomePageComponents/ShowrealSection/ShowrealSection.jsx
@@ -1,15 +1,29 @@
+"use client";
+
+import * as basicLightbox from "basiclightbox";
+
 import Section from "@/components/Section/Section";
 import Container from "@/components/Container/Container";
 
 import css from "./ShowrealSection.module.scss";
 
 const ShowrealSection = () => {
+
+    // const instance = basicLightbox.create(`
+    //     <iframe src="https://www.youtube.com/watch?v=ri3kZI0Ixfs" width="560" height="315" frameborder="0"></iframe>
+    // `);
+
+    // document.querySelector('.img-video').onclick = () => {
+    //     instance.show();
+    // };
+    
     return (
         <div className={css.wrapper}>
             <Section>
                 <h2 className={css["visually_hidden"]}>Showreal</h2>
                 <div className={css.parent}>
                     <div className={css["video__item--1"]}>
+                        <div className={css["img-video"]}></div>
                         <div className={css.overlay}>
                             <h3>VIP Auto Tires & Service</h3>
                             <ul>
@@ -37,7 +51,7 @@ const ShowrealSection = () => {
                         </div>
                     </div>
                     <div className={css["video__item--4"]}>
-                        <div className={css.overlay}>
+                        <div className={css["overlay-large"]}>
                             <h3>VIP Auto Tires & Service</h3>
                             <ul>
                                 <li>eCommerce</li>
