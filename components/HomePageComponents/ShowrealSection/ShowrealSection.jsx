b2b03ef8fdada7578495a7036dd386409213793f
Aleksey-Dr <aprel181818@gmail.com>
2024-02-19 21:54:45 +0200

    Add grid with video
    Add grid with video

--- components/HomePageComponents/ShowrealSection/ShowrealSection.jsx
+++ components/HomePageComponents/ShowrealSection/ShowrealSection.jsx
@@ -1,29 +1,25 @@
 "use client";
 
-import * as basicLightbox from "basiclightbox";
+import VideoReact from "@/components/VideoReact/VideoReact";
+import WorksButtonAndModal from "@/components/WorksButtonAndModal/WorksButtonAndModal";
 
 import Section from "@/components/Section/Section";
-import Container from "@/components/Container/Container";
 
 import css from "./ShowrealSection.module.scss";
 
 const ShowrealSection = () => {
 
-    // const instance = basicLightbox.create(`
-    //     <iframe src="https://www.youtube.com/watch?v=ri3kZI0Ixfs" width="560" height="315" frameborder="0"></iframe>
-    // `);
-
-    // document.querySelector('.img-video').onclick = () => {
-    //     instance.show();
-    // };
-    
     return (
         <div className={css.wrapper}>
             <Section>
                 <h2 className={css["visually_hidden"]}>Showreal</h2>
                 <div className={css.parent}>
                     <div className={css["video__item--1"]}>
-                        <div className={css["img-video"]}></div>
+                        <div className={css["img-video"]}>
+                            <WorksButtonAndModal
+                                url="https://player.vimeo.com/video/912707190?h=a0e16f4890"
+                            />
+                        </div>
                         <div className={css.overlay}>
                             <h3>VIP Auto Tires & Service</h3>
                             <ul>
@@ -33,6 +29,11 @@ const ShowrealSection = () => {
                         </div>
                     </div>
                     <div className={css["video__item--2"]}>
+                        <div className={css["img-video"]}>
+                            <WorksButtonAndModal
+                                url="https://player.vimeo.com/video/912707190?h=a0e16f4890"
+                            />
+                        </div>
                         <div className={css.overlay}>
                             <h3>VIP Auto Tires & Service</h3>
                             <ul>
@@ -42,6 +43,11 @@ const ShowrealSection = () => {
                         </div>
                     </div>
                     <div className={css["video__item--3"]}>
+                        <div className={css["img-video"]}>
+                            <WorksButtonAndModal
+                                url="https://player.vimeo.com/video/912707190?h=a0e16f4890"
+                            />
+                        </div>
                         <div className={css.overlay}>
                             <h3>VIP Auto Tires & Service</h3>
                             <ul>
@@ -51,6 +57,11 @@ const ShowrealSection = () => {
                         </div>
                     </div>
                     <div className={css["video__item--4"]}>
+                        <div className={css["img-video"]}>
+                            <WorksButtonAndModal
+                                url="https://player.vimeo.com/video/912707190?h=a0e16f4890"
+                            />
+                        </div>
                         <div className={css["overlay-large"]}>
                             <h3>VIP Auto Tires & Service</h3>
                             <ul>
@@ -60,6 +71,11 @@ const ShowrealSection = () => {
                         </div>
                     </div>
                     <div className={css["video__item--5"]}>
+                        <div className={css["img-video"]}>
+                            <WorksButtonAndModal
+                                url="https://player.vimeo.com/video/912707190?h=a0e16f4890"
+                            />
+                        </div>
                         <div className={css.overlay}>
                             <h3>VIP Auto Tires & Service</h3>
                             <ul>
@@ -69,6 +85,11 @@ const ShowrealSection = () => {
                         </div>
                     </div>
                     <div className={css["video__item--6"]}>
+                        <div className={css["img-video"]}>
+                            <WorksButtonAndModal
+                                url="https://player.vimeo.com/video/912707190?h=a0e16f4890"
+                            />
+                        </div>
                         <div className={css.overlay}>
                             <h3>VIP Auto Tires & Service</h3>
                             <ul>
@@ -78,6 +99,11 @@ const ShowrealSection = () => {
                         </div>
                     </div>
                     <div className={css["video__item--7"]}>
+                        <div className={css["img-video"]}>
+                            <WorksButtonAndModal
+                                url="https://player.vimeo.com/video/912707190?h=a0e16f4890"
+                            />
+                        </div>
                         <div className={css.overlay}>
                             <h3>VIP Auto Tires & Service</h3>
                             <ul>
