b35e06eff1a5bd6ca8f3f73d78a0d19da5e535c6
Victoria Fedkova <63882255+Victoria-Fedkova@users.noreply.github.com>
2024-02-26 16:01:36 +0200

    add fixes to slider item

--- components/HomePageComponents/FeedbackSection/FeedbackItem/FeedbackItem.jsx
+++ components/HomePageComponents/FeedbackSection/FeedbackItem/FeedbackItem.jsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 export default function FeedbackItem({ item }) {
   const {
     // avatar,
-    name,
+    user_name,
     text,
   } = item;
   return (
     <div className={css.feedbackItem}>
       <div className={css.text}>
         <Paragraph content={text} />
-        <h2>{name}</h2>
+        <h2>{user_name}</h2>
       </div>
       {/* <div className={css.user}>
         <div>
