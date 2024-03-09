b171eb6ee6a0dfd583c066616ec22f884b889f4c
Stanislav Kintoriak <kintoriak@gmail.com>
2024-03-09 22:24:22 +0200

    Commit 1
    1) changed logo wrapper to p (no Link, no href to home);
    2) changed favicon;
    3) changed route from "postServices" to "postservices" (+ everywhere in code);
    4) fixed vimeo iframe attribute allow full screen;
    5) fixed hydration error (again =));
    6) added some positions to file robots.txt;
    7) created file sitemap.js - added there all required pages for indexation;

--- components/Header/Logo/Logo.jsx
+++ components/Header/Logo/Logo.jsx
@@ -1,18 +1,17 @@
 import css from "./Logo.module.scss";
 import LogoText from "./LogoText/LogoText";
 import LogoSvg from "./LogoSvg/LogoSvg";
-import Link from "next/link";
 
 const Logo = ({ isFixed }) => {
   return (
     <div className={css.logo}>
-      <Link href="/">
+      <p>
         {/* <svg className={isFixed ? css.hidden:css.visible} fill={"white"} width={"80px"} height={"70px"}>
         <use href={"/logo-sprite.svg#logo"} />
       </svg> */}
         <LogoSvg isFixed={isFixed} />
         <LogoText logoText="Post Production Studio" />
-      </Link>
+        </p>
     </div>
   );
 };
